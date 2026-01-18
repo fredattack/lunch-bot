3d2da5af3ebacc86bf261db406735a4561d39b58
frederic moras <frederik.moras@vlaanderen.be>
2026-01-18 20:46:21 +0100

    Remove unimplemented LunchManager scheduled tasks

--- routes/console.php
+++ routes/console.php
@@ -1,21 +1,20 @@
 <?php
 
-use App\Services\LunchManager;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::call(function (LunchManager $manager) {
-    $day = $manager->ensureTodayLunchDay();
-    if ($day) {
-        $manager->postDailyKickoff($day);
-    }
-})->dailyAt(config('lunch.post_time'))->timezone(config('lunch.timezone'));
-
-Schedule::call(function (LunchManager $manager) {
-    $manager->lockExpiredDays();
-})->everyMinute();
+// TODO: Implement scheduled tasks when LunchManager is created
+// Schedule::call(function (LunchManager $manager) {
+//     $day = $manager->ensureTodayLunchDay();
+//     if ($day) {
+//         $manager->postDailyKickoff($day);
+//     }
+// })->dailyAt(config('lunch.post_time'))->timezone(config('lunch.timezone'));
+//
+// Schedule::call(function (LunchManager $manager) {
+//     $manager->lockExpiredDays();
+// })->everyMinute();
